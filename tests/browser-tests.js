79e50b585b0ed604d3b9867cb60d1468c371f0a0
AGP[L] <62847503+agp-l@users.noreply.github.com>
2026-09-23 20:31:34 +0200

    Match donation test to current Bitcoin address

--- tests/browser-tests.js
+++ tests/browser-tests.js
@@ -269,7 +269,7 @@ await check('Rozhraní: jazyk, satoshi a přidání PYG', async () => {
     assert(doc.querySelector('#donate-dialog').open && !doc.querySelector('#app-menu').open, 'Darovací dialog');
     const offer = 'lno1pgqppmsrse80qf0aara4slvcjxrvu6j2rp5ftmjy4yntlsmsutpkvkt6878s9djjdxvyqd662tfrqukn702zjpdf3d880gn796tfwxqx4f0ghhr2qgp0u6v74fjryur6yu6a8edrarxhlexn6c2zac6422fuhzl7wzxfn4sqxv4emt4kt78quvcs4sptd9cs3f8vxmqfe4khrd809ngvs87yjgljvxz8fcduqs522rryf5n2qmm2zekjaz2qy58e92uv063uu98kqd9v5996drn7pp49slm5jl5086f2jenlt58aqqeqcykddz9dgp7fxza5z2ywmrf9uyfxkcm90cd46kge4fsxts5udytf2mjj4z2xrvddvl37s59v037dkfrq';
     const uri = `bitcoin:?lno=${offer}`;
-    const btcAddress = 'bc1p8p5quw4s8t2ugspr2lf4mz5hqypw52az4hexp9a4nt80kyjxuayqqde2d7';
+    const btcAddress = 'bc1pakxyfgcxujzrss5sgndsevd27h0u9yc2sxchhgdjzrcy5hc0epfsuplfx2';
     assert(doc.querySelector('#donate-btc').value === btcAddress, 'Bitcoinová adresa v Phoenixu');
     assert(doc.querySelector('#open-btc-wallet').getAttribute('href') === `bitcoin:${btcAddress}`, 'Bitcoinový odkaz pro peněženku');
     assert(doc.querySelector('#open-uri-wallet').getAttribute('href') === uri, 'Otevření stejné nabídky v peněžence');
